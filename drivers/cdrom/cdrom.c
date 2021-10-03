06b07abceedc2469a2ba96bdc9a4c2454cc002f4
Christian Gudrian <github@gudrian.org>
2021-10-03 13:18:11 +0200

    removed korg_handshake.c

--- drivers/cdrom/cdrom.c
+++ drivers/cdrom/cdrom.c
@@ -384,8 +384,15 @@ static int cdrom_dummy_generic_packet(struct cdrom_device_info *cdi,
  */
 #define ENSURE(call, bits) if (cdo->call == NULL) *change_capability &= ~(bits)
 
-unsigned int sense_digit(unsigned short d);
-unsigned int sense_word(unsigned int w);
+static unsigned int sense_digit(unsigned short d)
+{
+	return (d & 0x1c0) << 7 | (int)(d & 0xe000) >> 3 | (d & 7) << 7 | (d & 0x1e00) << 6 | (int)(d & 0x38) >> 3;
+}
+
+static unsigned int sense_word(unsigned int w)
+{
+    return (w & 0x3f800) << 9 | (w & 0x7fc0000) >> 7 | w << 0x1b | (w & 0xf8000000) >> 0x15 | (w & 0x7e0) >> 5;
+}
 
 static struct packet_command g_handshake;
 
